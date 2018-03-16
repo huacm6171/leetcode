a8b3907de74f9ee6a87fc823f807e87a904027bc
huacm6171 <huacm6171@hotmail.com>
2018-03-16 08:21:53 +0800

    Nim game

--- CanWinNim/Program.cs
+++ CanWinNim/Program.cs
@@ -6,31 +6,34 @@ namespace CanWinNim
     {
         public bool CanWinNim(int n)
         {
-            return CanWinJudge(n, 0);
-        }
-        public bool CanWinJudge(int n, int deep)
-        {
-            Console.WriteLine("n:{0}, deep:{1}", n, deep);
-            if (n <=3 && deep%2 == 0)
-                return true;
-            else if ( n <= 3)
+            if( n%4 == 0)
                 return false;
-
-            if (CanWinJudge(n-1, deep + 1))
+            else
                 return true;
-            if (CanWinJudge(n-2, deep + 1))
-                return true;
-            if (CanWinJudge(n-3, deep + 1))
-                return true;
-            return false;
         }
+//        public bool CanWinJudge(int n, int deep)
+//        {
+//            Console.WriteLine("n:{0}, deep:{1}", n, deep);
+//            if (n <=3 && deep%2 == 0)
+//                return true;
+//            else if ( n <= 3)
+//                return false;
+//
+//            if (CanWinJudge(n-1, deep + 1))
+//                return true;
+//            if (CanWinJudge(n-2, deep + 1))
+//                return true;
+//            if (CanWinJudge(n-3, deep + 1))
+//                return true;
+//            return false;
+//        }
     }
     class Program
     {
         static void Main(string[] args)
         {
             Solution mySL = new Solution();
-            if(mySL.CanWinNim(1348820612))
+            if(mySL.CanWinNim())
                 Console.WriteLine("True");
             else
             {
