dfa1bfa905144857f0badb4526233fd418ae4407
huacm6171 <huacm6171@hotmail.com>
2018-04-01 09:36:02 +0800

    561

---
+++ ArrayPairSum_561/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayPairSum_561
+{
+    public class Solution
+    {
+        public int ArrayPairSum(int[] nums)
+        {
+            int iSum = 0;
+            Array.Sort(nums);
+            for (int i = 0; i < nums.Length; i+=2)
+            {
+                iSum += nums[i];
+            }
+            return iSum;
+        }
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
