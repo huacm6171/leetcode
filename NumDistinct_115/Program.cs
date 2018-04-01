47df9d85e7000bd9a29fec9e5069cbcc87ba8166
huacm6171 <huacm6171@hotmail.com>
2018-04-01 18:08:42 +0800

    计数二进制子串

---
+++ NumDistinct_115/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumDistinct_115
+{
+    class Program
+    {
+        public static int NumDistinct(string s, string t)
+        {
+            return 0;
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
