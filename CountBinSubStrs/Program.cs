dad851ef877af2c0e2091ca2949077d1d3a8a648
huacm6171 <huacm6171@hotmail.com>
2018-04-03 20:40:10 +0800

    MinSubArrayLen_209

--- CountBinSubStrs/Program.cs
+++ CountBinSubStrs/Program.cs
@@ -6,11 +6,40 @@ using System.Threading.Tasks;
 
 namespace CountBinSubStrs
 {
+    
 
     class Program
     {
+        public static int CountBinSubStrings(string s)
+        {
+            char preAlpha;
+            int cnt = 0;
+            int preAlphaCnt = 0;
+            int nexAlphaCnt = 0;
+            preAlpha = s[0];
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == preAlpha && nexAlphaCnt == 0)
+                    preAlphaCnt++;
+                else if (s[i] != preAlpha)
+                    nexAlphaCnt++;
+                else
+                {
+                    cnt += preAlphaCnt < nexAlphaCnt ? preAlphaCnt : nexAlphaCnt;
+                    preAlpha = preAlpha == '0' ? '1' : '0';
+                    preAlphaCnt = nexAlphaCnt;
+                    nexAlphaCnt = 1;
+                }
+            }
+            cnt += preAlphaCnt < nexAlphaCnt ? preAlphaCnt : nexAlphaCnt;
+            return cnt;
+        }
         static void Main(string[] args)
         {
+            string s = "10101";
+            int n;
+            n = CountBinSubStrings(s);
+            Console.WriteLine("{0}", n);
         }
     }
 }
---
+++ MinSubArrayLen/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinSubArrayLen
+{
+    class Program
+    {
+        public static int MinSubArrayLen(int s, int[] nums)
+        {
+            int sum = 0;
+            int start = 0;
+            //int end = 0;
+            int minLen = Int32.MaxValue;
+            for (int end = 0; end < nums.Length; end++)
+            {
+                sum += nums[end];
+                if(sum >= s)
+                {
+                    minLen = Math.Min(minLen, end - start + 1);
+                    while (start <= end&& sum >=s)
+                    {
+                        minLen = Math.Min(minLen, end - start + 1);
+                        sum -= nums[start];
+                        start++;
+                        
+                    }
+                    
+                }
+
+            }
+            if (minLen == Int32.MaxValue)
+            {
+                return -1;
+            }
+            return minLen;
+        }
+        static void Main(string[] args)
+        {
+            int[] nums = new int[]{ 1,4,4};
+            int s = 4;
+            int minlen;
+            minlen = MinSubArrayLen(s, nums);
+            return;
+        }
+    }
+}
