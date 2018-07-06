2638c07e8ede7b9aadecc4b76d4aade6cac48faf
huacm6171 <huacm6171@hotmail.com>
2018-07-06 21:25:41 +0800

    Rorate

---
+++ leetcode/Solutions/IsValidSudoku.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode.Solutions
+{
+    public  class IsValidSudoku
+    {
+
+        public bool Solution1(char[,] board)
+        {
+            Dictionary<char, int> dictCnt = new Dictionary<char, int>();
+            bool bRet = true;
+            for (int i = 0; i < 9; i++)
+            {
+                dictCnt.Clear();
+                if (!bRet)
+                {
+                    break;
+                }
+
+                for (int j = 0; j < 9; j++)
+                {
+                    if (board[i, j] != '.')
+                    {
+                        if (dictCnt.ContainsKey(board[i, j]))
+                        {
+                            bRet = false;
+                            break;
+                        }
+                        else
+                            dictCnt.Add(board[i, j], 1);
+                    }
+                }
+            }
+            //remove all elements of dict
+           
+            for (int j = 0; j < 9; j++)
+            {
+                dictCnt.Clear();
+                if (!bRet)
+                {
+                    break;
+                }
+                for (int i = 0; i < 9; i++)
+                {
+                    if (board[i, j] != '.')
+                    {
+                        if(dictCnt.ContainsKey(board[i, j]))
+                        {
+                            bRet = false;
+                            break;
+                        }
+                        else
+                            dictCnt.Add(board[i, j], 1);
+                    }           
+                 }
+            }
+
+            dictCnt.Clear();
+            for (int i = 0; i < 3; i++)
+            {
+                if (!bRet)
+                    break;
+
+                for (int j = 0; j < 3; j++)
+                {
+                    dictCnt.Clear();
+                    if (!bRet)
+                        break;
+                    for (int n = 0; n < 9; n++)
+                    {
+                        int x = i * 3 + n%3;
+                        int y = j * 3 + n / 3;
+                        if (board[x, y] != '.')
+                        {
+                            if (dictCnt.ContainsKey(board[x, y]))
+                            {
+                                bRet = false;
+                                break;
+                            }
+                            else
+                                dictCnt.Add(board[x, y], 1);
+                        }
+                  
+                    }
+                }
+            }
+            return bRet;
+        }
+    }
+}
---
+++ leetcode/Solutions/RotateMatrix.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode.Solutions
+{
+    public class RotateMatrix
+    {
+        public void Solution1(int[,] matrix)
+        {
+            int n = matrix.GetLength(1);
+            int tmp;
+            //旋转公式：a[i,j]->a[j,n-i-1]->a[n-i-1,n-j-1]->a[n-j-1,i]->a[i,j],每个循环可对每层的四个元素位置进行移动。
+            for (int i = 0; i < (n+1)/2; i++)
+            {
+                for (int j = 0; j < n-2*i -1; j++)
+                {
+                    tmp = matrix[i, i + j];
+                    matrix[i, i + j] = matrix[n - i - j - 1, i];
+                    matrix[n - i - j - 1, i] = matrix[n - i - 1, n - i - j - 1];
+                    matrix[n - i - 1, n - i - j - 1] = matrix[i + j, n - i - 1];
+                    matrix[i + j, n - i - 1] = tmp;
+                }
+            }
+        }
+    }
+}
