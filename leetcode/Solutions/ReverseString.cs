29140f4257478004bfaf460501df29cd7a91b110
huacm6171 <huacm6171@hotmail.com>
2018-07-08 09:58:23 +0800

    reversestr

---
+++ leetcode/Solutions/ReverseString.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode.Solutions
+{
+    public class ReverseString
+    {
+        public string Solution1(string s)
+        {
+            int n = s.Length;
+            char[] ch = s.ToCharArray();
+            char tmp;
+            for (int i = 0; i < n/2; i++)
+            {
+                tmp = ch[i];
+                ch[i] = ch[n - i - 1];
+                ch[n - i - 1] = tmp;
+            }
+            return new string(ch);
+        }
+    }
+}
